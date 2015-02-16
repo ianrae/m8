2322ec1dfe31a910d259e0902418231b315437fc
ianrae <winfell808@gmail.com>
2015-02-16 15:32:02 -0500

    more

--- app/controllers/SampleController.java
+++ app/controllers/SampleController.java
@@ -6,8 +6,14 @@ import javax.inject.Inject;
 
 import models.Sample;
 import models.dao.SampleDAO;
+import play.Logger;
+import play.Logger.ALogger;
+import play.data.Form;
 import play.mvc.Call;
+import play.mvc.Result;
 import play.utils.crud.CRUDController;
+import play.utils.crud.Parameters;
+import play.utils.dao.DAO;
 
 public class SampleController extends CRUDController<Long, Sample> {
 	
@@ -35,5 +41,41 @@ public class SampleController extends CRUDController<Long, Sample> {
 	protected Call toIndex() {
 		return routes.Application.index();
 	}
+	
+	
+//	@Override
+//	protected Result badRequest(String template, Parameters params) {
+//		return badRequest(render(template, params));
+//	}
+	
+	
+	private static ALogger xlog = Logger.of(CRUDController.class);
+	
+	@Override
+	public Result create() {
+		if (xlog.isDebugEnabled())
+			xlog.debug("Xcreate() <-");
 
+		Form<Sample> form =  Form.form(Sample.class);
+		
+		Form<Sample> filledForm = form.bindFromRequest();
+		if (filledForm.hasErrors()) {
+			if (xlog.isDebugEnabled())
+				xlog.debug("Xvalidation errors occured: " + filledForm.errors());
+
+			return badRequest(renderForm(null, filledForm));
+		} else {
+			Sample model = filledForm.get();
+			
+			DAO<Long,Sample> dao = getDao();
+			dao.create(model);
+			if (xlog.isDebugEnabled())
+				xlog.debug("Xentity created");
+
+			Call index = toIndex();
+			if (xlog.isDebugEnabled())
+				xlog.debug("Xindex : " + index);
+			return redirect(index);
+		}
+	}	
 }
