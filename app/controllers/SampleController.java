ae74eb26fdf01a5309fbe483e4e6ffa9d022563b
ianrae <winfell808@gmail.com>
2015-02-16 19:16:02 -0500

    sworks

--- app/controllers/SampleController.java
+++ app/controllers/SampleController.java
@@ -2,13 +2,18 @@ package controllers;
 
 import static play.data.Form.*;
 
+import java.lang.reflect.Method;
+
 import javax.inject.Inject;
+
+import org.springframework.util.ReflectionUtils;
 
 import mef.validate.SampleTwixt;
 import models.Sample;
 import models.dao.SampleDAO;
 import play.Logger;
 import play.Logger.ALogger;
+import play.api.Play;
 import play.data.Form;
 import play.mvc.Call;
 import play.mvc.Result;
@@ -31,7 +36,7 @@ public class SampleController extends CRUDController<Long, Sample> {
 
 	@Override
 	protected String templateForForm() {
-		return "sampleForm";
+		return "SampleForm";
 	}
 
 	@Override
@@ -50,19 +55,31 @@ public class SampleController extends CRUDController<Long, Sample> {
 //		return badRequest(render(template, params));
 //	}
 	
-	
+	private static ALogger xlog = Logger.of(CRUDController.class);
 	
 	protected Content xrenderForm(Long key, Form<SampleTwixt> form) {
-		String formName = "SampleForm";
-		return render(formName, with(Long.class, key).and(SampleTwixt.class, form));
+		
+//		try {
+//			xlog.debug("A");
+////			xcall("views.html.SampleForm", "render", with(Long.class, key).and(SampleTwixt.class, form));
+//			xlog.debug("A");
+//		} catch (ClassNotFoundException e) {
+//			// TODO Auto-generated catch block
+//			e.printStackTrace();
+//		} catch (MethodNotFoundException e) {
+//			// TODO Auto-generated catch block
+//			e.printStackTrace();
+//		}
+		return render(templateForForm(), with(Long.class, key).and(Form.class, form));
 	}
 	
-	private static ALogger xlog = Logger.of(CRUDController.class);
 	
 	public Result newForm() {
 		if (xlog.isDebugEnabled())
 			xlog.debug("xnewForm() <-");
 		Form<SampleTwixt> form =  Form.form(SampleTwixt.class);
+		
+
 
 		return ok(xrenderForm(null, form));
 	}	
@@ -95,5 +112,44 @@ public class SampleController extends CRUDController<Long, Sample> {
 				xlog.debug("Xindex : " + index);
 			return redirect(index);
 		}
+	}	
+	
+	
+	@SuppressWarnings("unchecked")
+	protected <R> R xcall(String className, String methodName, Parameters params)
+			throws ClassNotFoundException, MethodNotFoundException {
+		if (xlog.isDebugEnabled())
+			xlog.debug("xxxcall <-");
+		if (xlog.isDebugEnabled())
+			xlog.debug("className : " + className);
+		if (xlog.isDebugEnabled())
+			xlog.debug("methodName : " + methodName);
+		if (xlog.isDebugEnabled())
+			xlog.debug("params : " + params);
+		
+		ClassLoader appClassloader = Play.classloader(Play.current());		
+		ClassLoader cl = appClassloader; //classLoader();
+		Object result = null;
+		Class<?> clazz = cl.loadClass(className);
+		if (xlog.isDebugEnabled())
+			xlog.debug("clazz : " + clazz);
+		
+		for(Method mm : clazz.getMethods())
+		{
+			xlog.debug(mm.toString());
+		}
+
+		Class<?>[] paramTypes = params.types();
+		Object[] paramValues = params.values();
+		Method method = ReflectionUtils.findMethod(clazz, methodName,
+				paramTypes);
+		if (xlog.isDebugEnabled())
+			xlog.debug("method : " + method);
+
+		if (method == null) {
+			throw new MethodNotFoundException(className, methodName, paramTypes);
+		}
+		result = null;
+		return (R) result;
 	}	
 }
--- app/mef/validate/SampleTwixt.java
+++ app/mef/validate/SampleTwixt.java
@@ -1,20 +1,25 @@
 package mef.validate;
 import org.mef.framework.metadata.*;
-import org.mef.framework.metadata.validate.ValContext;
+//import org.mef.framework.metadata.validate.ValContext;
 
-public class SampleTwixt implements ValueContainer
+public class SampleTwixt //implements ValueContainer
 {
 	public StringValue name;
+	
+	public SampleTwixt()
+	{
+		this("");
+	}
 	
 	public SampleTwixt(String namex)
 	{
 		this.name = new StringValue(namex);
 	}
 
-	@Override
-	public void validateContainer(ValContext arg0) 
-	{
-		arg0.validate(name);
-	}
+//	@Override
+//	public void validateContainer(ValContext arg0) 
+//	{
+//		arg0.validate(name);
+//	}
 
 }
