8f5b4f4a0e81502e510869b57149e92417069ea1
ianrae <winfell808@gmail.com>
2015-02-23 21:07:41 -0500

    copyFrom

--- app/mef/validate/TwixtForm.java
+++ app/mef/validate/TwixtForm.java
@@ -1,6 +1,7 @@
 package mef.validate;
 
 import java.lang.reflect.Field;
+import java.lang.reflect.Method;
 
 import org.mef.framework.metadata.Value;
 import org.mef.framework.metadata.ValueContainer;
@@ -9,16 +10,32 @@ import org.springframework.util.ReflectionUtils;
 
 public abstract class TwixtForm implements ValueContainer, ReflectionUtils.FieldCallback
 {
+	boolean inCtor;
+	private Object modelToCopyFrom;
+	
 	public TwixtForm()
 	{}
 
-	protected void initDeclaredFields()
+	protected void initFields()
 	{
+		inCtor = true;
 		ReflectionUtils.doWithFields(this.getClass(), this, ReflectionUtils.COPYABLE_FIELDS);
 	}
 
 	@Override
 	public void doWith(Field field)
+	{
+		if (inCtor)
+		{
+			initField(field);
+		}
+		else if (modelToCopyFrom != null)
+		{
+			copyFieldFromModel(field);
+		}
+	}
+	
+	private void initField(Field field)
 	{
 		Class<?> clazz = field.getType();
 		if (Value.class.isAssignableFrom(clazz))
@@ -36,20 +53,62 @@ public abstract class TwixtForm implements ValueContainer, ReflectionUtils.Field
 		}
 	}
 
+	private void copyFieldFromModel(Field field)
+	{
+		Class<?> clazz = field.getType();
+		if (Value.class.isAssignableFrom(clazz))
+		{
+			try 
+			{
+				String getFnName = "get" + uppify(field.getName());
+				Method meth = ReflectionUtils.findMethod(this.modelToCopyFrom.getClass(), getFnName);
+				if (meth != null)
+				{
+					Object src = meth.invoke(this.modelToCopyFrom);
+					
+					getFnName = "forceValueObject";
+					meth = ReflectionUtils.findMethod(clazz, getFnName, Object.class);
+					
+					field.setAccessible(true);
+					Object valueObj = field.get(this);
+					
+					meth.invoke(valueObj, src);
+				}
+			} 
+			catch (Exception e) 
+			{
+				e.printStackTrace();
+			}
+		}
+	}
+	
+	private String uppify(String name) 
+	{
+		String upper = name.toUpperCase();
+		String s = upper.substring(0, 1);
+		s += name.substring(1);
+		return s;
+	}	
+
 	@Override
 	public abstract void copyFrom(Object model);
 
 	@Override
 	public abstract void copyTo(Object model);
 
-	protected void copyFieldsFrom(Object model)
-	{}
-	protected void copyFieldsTo(Object model)
-	{}
-
 	@Override
 	public void validateContainer(ValContext arg0) 
 	{
 	}
 
+	//helpers
+	protected void copyFieldsFrom(Object model)
+	{
+		inCtor = false;
+		modelToCopyFrom = model;
+		ReflectionUtils.doWithFields(this.getClass(), this, ReflectionUtils.COPYABLE_FIELDS);
+	}
+	protected void copyFieldsTo(Object model)
+	{}
+	
 }
--- test/ValueTests.java
+++ test/ValueTests.java
@@ -14,12 +14,34 @@ public class ValueTests extends BaseTest
 	public static class MyForm extends TwixtForm
 	{
 		StringValue s;
-		IntegerValue a;
+		IntegerValue abc;
 		BooleanValue b;
 		
 		public MyForm()
 		{
-			this.initDeclaredFields();
+			this.initFields();
+		}
+
+		@Override
+		public void copyFrom(Object model) 
+		{
+			this.copyFieldsFrom(model);
+		}
+
+		@Override
+		public void copyTo(Object model) {
+			// TODO Auto-generated method stub
+			
+		}
+	}
+	
+	public static class MyDerivedForm extends MyForm
+	{
+		StringValue d;
+		
+		public MyDerivedForm()
+		{
+			this.initFields(); //double init -- can we avoid this??
 		}
 
 		@Override
@@ -35,14 +57,59 @@ public class ValueTests extends BaseTest
 		}
 	}
 	
+	public static class MyModel 
+	{
+		String s;
+		int abc;
+		boolean b;
+		
+		public String getS()
+		{
+			return s;
+		}
+		public int getAbc()
+		{
+			return abc;
+		}
+		public boolean getB()
+		{
+			return b;
+		}
+	}
+	
 	
 	@Test
 	public void test() 
 	{
 		log("a");
 		MyForm frm = new MyForm();
-		assertNull(frm.s);
-		
+		assertNotNull(frm.s);
+		assertNotNull(frm.abc);
+		assertNotNull(frm.b);
 	}
 
+	@Test
+	public void testDerived() 
+	{
+		MyDerivedForm frm = new MyDerivedForm();
+		assertNotNull(frm.s);
+		assertNotNull(frm.abc);
+		assertNotNull(frm.b);
+	}
+	
+	@Test
+	public void testCopy() 
+	{
+		MyModel m = new MyModel();
+		m.abc = 45;
+		m.b = true;
+		m.s = "def";
+		
+		MyForm frm = new MyForm();
+		frm.copyFrom(m);
+		
+		assertEquals(45, frm.abc.get());
+		assertEquals(true, frm.b.get());
+		assertEquals("def", frm.s.get());
+	}
 }
