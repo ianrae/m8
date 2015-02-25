bdd84f989d58b352c4fe5f93dbc4efa4a5338c09
ianrae <winfell808@gmail.com>
2015-02-24 20:39:49 -0500

    facade

--- app/mef/validate/TwixtForm.java
+++ app/mef/validate/TwixtForm.java
@@ -7,40 +7,56 @@ import org.mef.framework.metadata.ValueContainer;
 import org.mef.framework.metadata.validate.ValContext;
 import org.springframework.util.ReflectionUtils;
 
-public abstract class TwixtForm implements ValueContainer, ReflectionUtils.FieldCallback, FormCopier.FieldCopier
+public abstract class TwixtForm implements ValueContainer
 {
+	private class Facade implements  FormCopier.FieldCopier, ReflectionUtils.FieldCallback
+	{
+		@Override
+		public void doWith(Field field)
+		{
+			Class<?> clazz = field.getType();
+			if (Value.class.isAssignableFrom(clazz))
+			{
+				try 
+				{
+					field.setAccessible(true);
+					if (field.get(TwixtForm.this) != null)
+					{
+						return; //skip ones that are already not null
+					}
+					
+					Object obj = clazz.newInstance();
+					field.set(TwixtForm.this, obj);
+				} 
+				catch (Exception e) 
+				{
+					e.printStackTrace();
+				}
+			}
+		}
+		
+		@Override
+		public void copyFieldFromModel(FormCopier copier, Field field)
+		{
+			copier.copyFieldFromModel(field);
+		}
+		@Override
+		public void copyFieldToModel(FormCopier copier, Field field)
+		{
+			copier.copyFieldToModel(field);
+		}
+	}
+	
+	private Facade _facade = new Facade(); //avoid name clash, use _
+	
 	public TwixtForm()
 	{}
 
 	protected void initFields()
 	{
-		ReflectionUtils.doWithFields(this.getClass(), this, ReflectionUtils.COPYABLE_FIELDS);
+		ReflectionUtils.doWithFields(this.getClass(), _facade, ReflectionUtils.COPYABLE_FIELDS);
 	}
 
-	@Override
-	public void doWith(Field field)
-	{
-		Class<?> clazz = field.getType();
-		if (Value.class.isAssignableFrom(clazz))
-		{
-			try 
-			{
-				field.setAccessible(true);
-				if (field.get(this) != null)
-				{
-					return; //skip ones that are already not null
-				}
-				
-				Object obj = clazz.newInstance();
-				field.set(this, obj);
-			} 
-			catch (Exception e) 
-			{
-				e.printStackTrace();
-			}
-		}
-	}
-	
 	
 	@Override
 	public void copyFrom(Object model) 
@@ -56,25 +72,15 @@ public abstract class TwixtForm implements ValueContainer, ReflectionUtils.Field
 	
 	protected void copyFieldsFromModel(Object model, String... fieldsToNotCopy) 
 	{
-		FormCopier copier = new FormCopier(this);
+		FormCopier copier = new FormCopier(_facade);
 		copier.copyFromModel(model, this, fieldsToNotCopy);
 	}
 	protected void copyFieldsToModel(Object model, String... fieldsToNotCopy) 
 	{
-		FormCopier copier = new FormCopier(this);
+		FormCopier copier = new FormCopier(_facade);
 		copier.copyToModel(this, model, fieldsToNotCopy);
 	}
 	
-	@Override
-	public void copyFieldFromModel(FormCopier copier, Field field)
-	{
-		copier.copyFieldFromModel(field);
-	}
-	@Override
-	public void copyFieldToModel(FormCopier copier, Field field)
-	{
-		copier.copyFieldToModel(field);
-	}
 
 	@Override
 	public void validateContainer(ValContext arg0) 
